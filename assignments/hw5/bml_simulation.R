c19c671caf16ce83ad05064f1fadbaa60a52ffa5
John Asirvatham <jasirvatham@berkeley.edu>
2015-03-14 23:57:59 -0700

    actual finished project

--- assignments/hw5/bml_simulation.R
+++ assignments/hw5/bml_simulation.R
@@ -6,9 +6,11 @@
 #### when you write up your results.
 #### The output can e.g. be how many steps the system took until it hit gridlock or
 #### how many steps you observered before concluding that it is in a free flowing state.
-
+counter = 0
 for (i in 1:100) {
     if(!bml.sim(5,4,.9)) {
         print (i)
+        counter = counter + 1
     }
 }
+print(paste0("It gridlocked ", counter, " times"))
