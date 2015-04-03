5bbc00a6714f4fbfd83e86372815cb3c4a8601fb
johnasir <jallan95@gmail.com>
2015-04-02 17:18:42 -0700

    midterm2

--- midterm2/midterm2.r
+++ midterm2/midterm2.r
@@ -4,23 +4,26 @@
 ## General R commands
 
 # [1 pt]
-# Create [x], a numeric vector of length 1000 with 
+# Create [x], a numeric vector of length 2000 with 
 # entries: 6, 12, 18, etc.
+present <- "yes"
 
-#x <- <your code here>
+x <- seq(1,2000) * 6
 
 
 # [1 pt]
 # Create [y], a logical vector of length 2000 
 # with y[i]=T if x[i] is divisible by 10, otherwise F
 
-# y <- <your code here>
+y <- x %% 10 == 0
+
 
 # [1 pt]
 # Create [w], a random permutation of the numeric values of a deck of cards
 # (i.e. just the numbers 1 through 13 each repeated 4 times)
 set.seed(2718)
-#w <- <your code here>
+w <- sample(1:13, 52, TRUE)
+
 
 
 # [1 pt]
@@ -29,14 +32,14 @@ set.seed(2718)
 # (arrange the values by column, as per default)
 set.seed(344)
 
-#m <- <your code here>
+m <- matrix(rexp(100, 3), nrow = 10)
 
 
 # [1 pt]
 # Create [l], a list with 12 elements, each a vector of length 100.
 # Each vector of length 100 of Poisson (hint:rpois) random variables with mean 5
 set.seed(71)
-#l <- <your code here>
+l = list(rpois(100,5),rpois(100,5),rpois(100,5),rpois(100,5),rpois(100,5),rpois(100,5),rpois(100,5),rpois(100,5),rpois(100,5),rpois(100,5),rpois(100,5),rpois(100,5))
 
 
 # for the next two tasks you will use the data frame infants (size 1236x15)
@@ -45,12 +48,11 @@ load("KaiserBabies.rda")
 
 # [2 pt]
 # Create a table [t] of the education level ($ed) of all married ($marital) first time ($parity=1) mothers:
-#t <- <your code here>
-
-
+t <- table(subset(infants, marital = "Married" & parity == 1)$ed)
 # [2 pt]
 # Calculate [mw], the average birthweight ($bwt) of all babies whose were full term, i.e. gestation equal or more than 259 days.
-#mw <- <your code here>
+mw <- mean(subset(infants, gestation >= 259)$bwt, na.rm = T)
+
 
 
 # For the next few tasks you will use the data frame family (size 14x5)
@@ -59,28 +61,31 @@ load("family.rda")
 
 # [1 pt]
 # Create [f1] a subset of family with only women over age 50
-#f <- <your code here>
+f1 <- subset(family, gender == "f" & age > 50)
+
 
   
+f1
 # [1 pt]
 # Create [f2] a subset of family with only men 6 foot tall or more
-#fm <- <your code here>
+f2 <- subset(family, gender == "m" & height >= 72)
 
   
 # [1 pt]
 # Create [f3] a subset of family of people whose name starts with T
-#f3 <- <your code here>
+f3 <- subset(family, substring(name,1,1) == "T")
+
   
 
 
 # [1 pt]
 # Create [f4] a subset of family with just the youngest individual (so just one row)
-#f4 <- <your code here>
+f4 <- subset(family, age == min(family$age))
 
 
 
 
-## Plotting
+ ## Plotting
 
 # We will now use the dataset "iris" which is icluded in the R package.
 # To look at the dataframe you can just type "iris" at the prompt
@@ -89,7 +94,7 @@ load("family.rda")
 
 # [2 pts]
 # Make a box plot of Sepal Length by Species (so 3 boxplots in one plot)
-
+boxplot(iris$Sepal.Length)
 
 
 # [3 pts]
